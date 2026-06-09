f6db04436fa620856f2cb9001c4f32e703e5687e
Charles Moulliard <cmoulliard@ibm.com>
2026-06-09 10:21:37 +0200

    Add new languages to the enum
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- core/src/main/java/io/roastedroot/treesitter/Language.java
+++ core/src/main/java/io/roastedroot/treesitter/Language.java
@@ -2,7 +2,12 @@ package io.roastedroot.treesitter;
 
 public enum Language {
     JSON(0),
-    JAVA(1);
+    JAVA(1),
+    PROPERTIES(2),
+    HTML(3),
+    XML(4),
+    MARKDOWN(5),
+    YAML(6);
 
     private final int id;
 
